9173f2a6f075c6405131f354dcb5b11c61c27c7a
Mehdi Zymberi <m.zymberi@student.avans.nl>
2023-09-10 14:30:36 +0200

    SideBar Added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
-import './App.css';
+import "./App.css";
+import gptLogo from "./assets/chatgpt.svg";
+import addBtn from "./assets/add-30.png";
+import msgIcon from "./assets/message.svg";
+import homeIcon from "./assets/home.svg";
+import saveIcon from "./assets/bookmark.svg";
+import upgradeIcon from "./assets/rocket.svg";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <div className="sideBar">
+        <div className="upperSide">
+          <div className="upperSideTop">
+            <img src={gptLogo} alt="Logo" className="logo" />
+            <span className="brand">Buci GPt</span>
+          </div>
+          <button className="midBtn">
+            <img src={addBtn} alt="" className="addBtn" />
+            New Chat
+          </button>
+          <div className="upperSideBottom">
+            <button className="query">
+              <img src={msgIcon} alt="Qeury" />
+              What is programming ?
+            </button>
+            <button className="query">
+              <img src={msgIcon} alt="Qeury" />
+              How to suck at everything ?
+            </button>
+          </div>
+        </div>
+        <div className="lowerSide">
+         
+            <div className="listItems"><img src={homeIcon} alt="Home" className="listItemImg" /> Home</div>
+            <div className="listItems"> <img src={saveIcon} alt="Saved" className="listItemImg" /> Saved</div>
+            <div className="listItems"><img src={upgradeIcon} alt="Upgrade" className="listItemImg" /> Upgrade</div>
+     
+        </div>
+      </div>
+      <div className="main"></div>
     </div>
   );
 }
