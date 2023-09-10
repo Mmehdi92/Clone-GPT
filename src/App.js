05ba3135873078512a08cba3f451475a47639565
Mehdi Zymberi <m.zymberi@student.avans.nl>
2023-09-10 16:03:01 +0200

    Add User - GptBot Sementics

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import msgIcon from "./assets/message.svg";
 import homeIcon from "./assets/home.svg";
 import saveIcon from "./assets/bookmark.svg";
 import upgradeIcon from "./assets/rocket.svg";
+import sendIcon from "./assets/send.svg";
+import userIcon from "./assets/user-icon.png";
+import gptImgLogo from "./assets/chatgptLogo.svg";
 
 function App() {
   return (
@@ -26,19 +29,48 @@ function App() {
             </button>
             <button className="query">
               <img src={msgIcon} alt="Qeury" />
-              How to suck at everything ?
+              How to ... ?
             </button>
           </div>
         </div>
         <div className="lowerSide">
-         
-            <div className="listItems"><img src={homeIcon} alt="Home" className="listItemImg" /> Home</div>
-            <div className="listItems"> <img src={saveIcon} alt="Saved" className="listItemImg" /> Saved</div>
-            <div className="listItems"><img src={upgradeIcon} alt="Upgrade" className="listItemImg" /> Upgrade</div>
-     
+          <div className="listItems">
+            <img src={homeIcon} alt="Home" className="listItemImg" /> Home
+          </div>
+          <div className="listItems">
+            {" "}
+            <img src={saveIcon} alt="Saved" className="listItemImg" /> Saved
+          </div>
+          <div className="listItems">
+            <img src={upgradeIcon} alt="Upgrade" className="listItemImg" />{" "}
+            Upgrade
+          </div>
         </div>
       </div>
-      <div className="main"></div>
+      <div className="main">
+        <div className="chats">
+          <div className="chat">
+            <img className="chatImg" src={userIcon} alt="" />
+            <p className="text">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum nemo temporibus ratione deleniti eaque architecto illo sit itaque laboriosam quaerat. Labore eius quo rerum dolorum className="chatImg" 
+            </p>
+          </div>
+          <div className="chat bot">
+            <img className="chatImg" src={gptImgLogo} alt="" />
+            <p className="text">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi eveniet numquam dignissimos necessitatibus. Illo placeat quidem dolorem vitae est vero quaerat temporibus! Cumque nam, esse totam dolore fuga libero itaque dolores laborum minima modi, voluptate nisi molestiae eligendi sequi autem delectus deleniti sit aspernatur temporibus consectetur nemo facere necessitatibus. Perferendis quas iste dolore vitae repellendus soluta reiciendis similique in eum natus illum deleniti nemo maiores temporibus pariatur sit eaque aperiam beatae veritatis, eveniet itaque expedita facere saepe. Reprehenderit quibusdam optio commodi, numquam magni beatae ex iste ipsam quidem voluptates itaque inventore natus ipsum aliquam hic excepturi alias velit animi mollitia!
+            </p>
+          </div>
+        </div>
+        <div className="chatFooter">
+          <div className="inp">
+            <input type="text" placeholder="Send A Message ..." />
+            <button className="send">
+              <img src={sendIcon} alt="Send" />
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
